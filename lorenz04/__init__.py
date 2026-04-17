1802811f9c130d90219be4b474d76b87ac134795
jswhit2 <jwhitaker@Jeffreys-MacBook-Air.local>
2026-04-17 07:27:10 -0600

    update

--- lorenz04/__init__.py
+++ lorenz04/__init__.py
@@ -1,3 +1,3 @@
-from .enkf_utils import gaspcohn, cartdist, lgetkf, lgetkf_ms,getkf_bloc,getkfms_bloc
+from .enkf_utils import gaspcohn, cartdist, lgetkf, lgetkf_ms,getkf_bloc,getkfms_bloc,lgetkfms_bloc
 from .lorenz04 import Lorenz04
-__all__=['Lorenz04',gaspcohn,cartdist,lgetkf,lgetkf_ms,getkf_bloc,getkfms_bloc]
+__all__=['Lorenz04',gaspcohn,cartdist,lgetkf,lgetkf_ms,getkf_bloc,getkfms_bloc,lgetkfms_bloc]
